02a905cc9d9e221a43acb9aae8746d8eeae0edb7
TJ Bates <tjbates@wisc.edu>
2023-04-16 16:46:50 -0500

    Added account page and fixed page links

--- public/app.js
+++ public/app.js
@@ -116,3 +116,22 @@ firebase.auth().onAuthStateChanged((user) => {
   }
 });
 
+// Select the input element using
+// document.querySelector
+  var input = document.querySelector(
+    "#file-upload>.file-label>.file-input"
+  );
+
+// Bind an listener to onChange event of the input
+  input.onchange = function () {
+      if(input.files.length > 0){
+          var fileNameContainer =
+              document.querySelector(
+                "#file-upload>.file-label>.file-name"
+              );
+          // set the inner text of fileNameContainer
+          // to the name of the file
+          fileNameContainer.textContent =
+            input.files[0].name;
+      }
+  }
